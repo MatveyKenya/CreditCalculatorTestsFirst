e1f3550858eae1701007a75f8c4729438d6cbd8d
Matvey Kenya <matveykenya@gmail.com>
2021-10-23 11:26:01 +0500

    №2 наполнили класс формулами тесты сработали

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -9,20 +9,20 @@ public class CreditCalculator {
 
     //расчет месячного платежа
     public double monthPayment(double sumCredit, double yearRate, int countMonths){
-
-        return 0;
+        double monthRate = yearRate / 12 / 100;
+        return sumCredit * (monthRate + monthRate / (Math.pow(1 + monthRate, countMonths) - 1));
     }
 
     //расчет общей суммы возврата в банк
     public double totalPayment(double sumCredit, double yearRate, int countMonths){
 
-        return 0;
+        return monthPayment(sumCredit, yearRate, countMonths) * countMonths;
     }
 
     //Расчет переплаты за весь период
     public double overPayment(double sumCredit, double yearRate, int countMonths){
 
-        return 0;
+        return totalPayment(sumCredit, yearRate, countMonths) - sumCredit;
     }
 
 }
